2a5209d767b1a504f8f5d24b37cfe3f64ca987e3
Dylan Fischler <dylanfischler@gmail.com>
2016-12-05 23:24:58 -0500

    clustering and hull calculations might be working

--- Cluster.py
+++ Cluster.py
@@ -1,10 +1,11 @@
 import numpy as np
 from sklearn.cluster import DBSCAN
-
+from scipy.spatial import ConvexHull
 
 class Cluster():
     def __init__(self, locations):
         self.data_set = self.generateDataSet(locations)
+        self.cluster = None
 
     def generateDataSet(self, locations):
         data = []
@@ -19,4 +20,29 @@ class Cluster():
     def dbscan(self, eps=0.3, min_samples=None):
         # TODO: implement
         db = DBSCAN(eps, min_samples, n_jobs=2, algorithm='ball_tree', metric='haversine').fit(np.radians(self.data_set))
-        return db
+        self.cluster = db
+
+    def getClusterLists(self):
+        clusterDict = {}
+        for i, cluster in enumerate(self.cluster.labels_):
+            if cluster not in clusterDict:
+                clusterDict[cluster] = []
+            clusterDict[cluster].append(i)
+        return clusterDict
+
+    def getPoint(self, index):
+        return self.data_set[index].tolist()
+
+    def getClusterHull(self, clusterPoints):
+        data = [self.data_set[i] for i in clusterPoints]
+        return [self.getPoint(coord) for coord in ConvexHull(data).vertices]
+
+    def getClusterDetails(self):
+        clusterDict = self.getClusterLists()
+        detailedClusterDict = {}
+
+        for cluster in clusterDict:
+            detailedClusterDict[cluster] = {}
+            detailedClusterDict[cluster]["points"] = [self.getPoint(ind) for ind in clusterDict[cluster]]
+            detailedClusterDict[cluster]["hull"] = self.getClusterHull(clusterDict[cluster])
+        return detailedClusterDict
--- clusterdata.py
+++ clusterdata.py
@@ -2,6 +2,7 @@ import numpy as np
 import os, json
 from Cluster import Cluster
 import matplotlib.pyplot as plt
+from scipy.spatial import ConvexHull
 
 DATA_FILE = 'data/locations.json'
 
@@ -12,16 +13,28 @@ def processDataFile():
     return json.loads(data_file.read())
 
 locations = processDataFile()
-clusterInstance = Cluster(locations)
+dbscluster = Cluster(locations)
+data = dbscluster.data_set
+
 print("Running DBSCAN on data set...")
-dbscanClusters = clusterInstance.dbscan(eps=0.000006, min_samples=5)
-clusters = set(dbscanClusters.labels_)
-print("{} clusters found".format(len(clusters)))
-print(clusters)
+dbscluster.dbscan(eps=0.000006, min_samples=5)
+dbscanClusters = dbscluster.cluster
+clusterLists = dbscluster.getClusterLists()
 
+num_clusters = len(set(dbscanClusters.labels_))
+print("{} clusters found".format(num_clusters))
+
+clusterDetails = dbscluster.getClusterDetails()
+
+# plt.scatter(clusterInstance.data_set[:,0], clusterInstance.data_set[:,1])
+# hull = ConvexHull(clusterInstance.data_set)
+# clusters = [data[dbscanClusters.labels_ == i] for i in xrange(len(dbscanClusters.labels_))]
+# print(clusters)
+#
+# plt.plot(data[:,0], data[:,1], 'o')
+# for simplex in hull.simplices:
+#     plt.plot(data[simplex, 0], data[simplex, 1], 'k-')
+# plt.show()
+#
 clusterFile = open('clusters', 'w')
-clusterFile.write(json.dumps(dbscanClusters.labels_.tolist()))
-
-# display all points on a plot
-plt.scatter(clusterInstance.data_set[:,0], clusterInstance.data_set[:,1])
-plt.show()
+clusterFile.write(json.dumps(clusterDetails))
